a3474407029f4c14006b1aa0967096801f8f787a
alobikov <aleksej.lobikov@gmail.com>
2020-07-03 10:13:49 +0300

    deployment script and suppl. added

---
+++ notify-setup.sh
@@ -0,0 +1,33 @@
+# sudo apt update
+# check for Nodejs
+version=$(node -v)
+if [ $? ]; then
+    echo  "Nodejs version $version: OK"
+fi    
+
+# check for MongoDb
+version=$(sudo /usr/bin/mongod --version) 
+status=$?
+
+if [ $status = "1" ]; then 
+	echo "Installing MongoDb..."
+	sudo apt install mongodb -y
+elif [ $status = "0" ]; then
+	echo $version
+	echo "MongoDb: OK" 
+fi
+
+# installing systemd service file
+if [ -f notify.service ]; then
+	sudo cp notify.service /lib/systemd/system/
+	echo Notify service added to systemd
+	sudo systemctl daemon-reload
+	sudo systemctl start notify
+	echo Notify service started
+	sudo systemctl enable notify
+	echo Notify service autostart enabled
+else 
+	echo "*** Systemd service is missing ***"
+	exit 1
+fi
+
