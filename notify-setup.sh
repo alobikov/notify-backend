ccfc5766d37fbeeaa98f97a2f04f54e016059154
alobikov <aleksej.lobikov@gmail.com>
2020-07-17 11:16:06 +0300

    updatind deployment sript

--- notify-setup.sh
+++ notify-setup.sh
@@ -27,39 +27,39 @@ fi
   echo [Install] >> $file
   echo WantedBy=multi-user.target >> $file
 
-# # sudo apt update
-# # check for Nodejs
-# version=$(node -v)
-# if [ $? ]; then
-#     echo  "Nodejs version $version: OK"
-# else
-#    echo "Installing Node.js and npm..."
-#    sudo apt install nodejs npm
-# fi
+# sudo apt update
+# check for Nodejs
+version=$(node -v)
+if [ $? ]; then
+    echo  "Nodejs version $version: OK"
+else
+   echo "Installing Node.js and npm..."
+   sudo apt install nodejs npm
+fi
 
-# # check for MongoDb
-# version=$(sudo /usr/bin/mongod --version) 
-# status=$?
+# check for MongoDb
+version=$(sudo /usr/bin/mongod --version) 
+status=$?
 
-# if [ $status = "1" ]; then 
-# 	echo "Installing MongoDb..."
-# 	sudo apt install mongodb -y
-# elif [ $status = "0" ]; then
-# 	echo $version
-# 	echo "MongoDb: OK" 
-# fi
+if [ $status = "1" ]; then 
+	echo "Installing MongoDb..."
+	sudo apt install mongodb -y
+elif [ $status = "0" ]; then
+	echo $version
+	echo "MongoDb: OK" 
+fi
 
-# # installing systemd service file
-# if [ -f notify.service ]; then
-# 	sudo cp notify.service /lib/systemd/system/
-# 	echo Notify service added to systemd
-# 	sudo systemctl daemon-reload
-# 	sudo systemctl start notify
-# 	echo Notify service started
-# 	sudo systemctl enable notify
-# 	echo Notify service autostart enabled
-# else 
-# 	echo "*** Systemd service is missing ***"
-# 	exit 1
-# fi
+# installing systemd service file
+if [ -f notify.service ]; then
+	sudo cp notify.service /lib/systemd/system/
+	echo Notify service added to systemd
+	sudo systemctl daemon-reload
+	sudo systemctl start notify
+	echo Notify service started
+	sudo systemctl enable notify
+	echo Notify service autostart enabled
+else 
+	echo "*** Systemd service is missing ***"
+	exit 1
+fi
 
